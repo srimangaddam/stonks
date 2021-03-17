4e2e26a5eac83fcb7cf999a61855cca2507dac99
Sriman Gaddam <srimangaddam@gmail.com>
2021-03-17 01:12:30 -0500

    Added Component

---
+++ src/Components/Search.js
@@ -0,0 +1,6 @@
+export default function search() {
+    return (
+    <div>
+        <h2>Watchlist</h2>
+    </div>)
+}
---
+++ src/Components/Watchlist.js
@@ -0,0 +1,6 @@
+export default function watchlist() {
+    return (
+    <div>
+        <h2>Watchlist</h2>
+    </div>)
+}
