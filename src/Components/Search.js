71c97aa95ff182031b31753d454d9f133295aa39
Sriman Gaddam <srimangaddam@gmail.com>
2021-03-17 03:52:50 -0500

    Prototype Search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,68 +1,24 @@
-import React from "react";
-import { render } from "react-dom";
-import { TransitionMotion, spring } from "react-motion";
-import "./search.css";
+import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react';
 
-export default class Search extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      active: (props.locked && props.active) || false,
-      value: props.value || "",
-      error: props.error || "",
-      label: props.label || "Label"
+ export default function Search(props) {
+    const nameEl = React.useRef(null);
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      alert(nameEl.current.value);
     };
-  }
-
-  changeValue(event) {
-    const value = event.target.value;
-    this.setState({ value, error: "" });
-  }
-
-  handleKeyPress(event) {
-    if (event.which === 13) {
-      this.setState({ value: this.props.predicted });
-    }
-  }
-
-  render() {
-    const { active, value, error, label } = this.state;
-    const { predicted, locked } = this.props;
-    const fieldClassName = `field ${(locked ? active : active || value) &&
-      "active"} ${locked && !active && "locked"}`;
-
+  
     return (
-      <div className={fieldClassName}>
-        {active &&
-          value &&
-          predicted &&
-          predicted.includes(value) && <p className="predicted">{predicted}</p>}
-        <input
-          id={1}
-          type="text"
-          value={value}
-          placeholder={label}
-          onChange={this.changeValue.bind(this)}
-          onKeyPress={this.handleKeyPress.bind(this)}
-          onFocus={() => !locked && this.setState({ active: true })}
-          onBlur={() => !locked && this.setState({ active: false })}
-        />
-        <label htmlFor={1} className={error && "error"}>
-          {error || label}
-        </label>
-      </div>
-    );
+       <form>
+            <TextField onChange={handleSubmit} id="standard-basic" label="Standard" />
+    
+       </form>
+     );
   }
-}
 
-render(
-  <Search
-    id={1}
-    label="Field label"
-    predicted="California"
-    locked={false}
-    active={false}
-  />,
-  document.getElementById("root")
-);
+  /* <label>Name:
+  <input type="text" ref={nameEl} />
+  </label>
+  <input type="submit" name="Submit" /> 
+  */
